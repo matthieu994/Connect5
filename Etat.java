8e1e7428cdf5a7a45fcbf5d049029bd16e9ae554
Matthieu <matthieu@notebook>
2019-04-05 23:57:20 -0400

    refactor all

---
+++ Etat.java
@@ -0,0 +1,206 @@
+package Connect5Game;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.AbstractMap.SimpleEntry;
+
+import Connect5Game.Groupe;
+import Connect5Game.Noeud;
+
+public class Etat implements Comparable<Etat> {
+    final byte[][] grille;
+
+    public ArrayList<Groupe> listeGroupes;
+    public ArrayList<Groupe> listeGroupesAdversaire;
+
+    Noeud[][] noeuds;
+    Noeud[][] noeudsAdversaire;
+
+    int joueur; // Contient le joueur courant
+    int adversaire;
+
+    public Etat(byte[][] grille, int joueur) {
+        this.grille = grille;
+
+        listeGroupes = new ArrayList<>();
+        listeGroupesAdversaire = new ArrayList<>();
+
+        noeuds = new Noeud[grille.length][grille[0].length];
+        noeudsAdversaire = new Noeud[grille.length][grille[0].length];
+
+        this.joueur = joueur;
+        this.adversaire = (joueur == 1) ? 2 : 1;
+    }
+
+    public Etat(Etat etat) {
+        this(etat.grille, etat.joueur);
+    }
+
+    public Etat genererSuccesseur(int ligne, int colonne) {
+        this.genererEtat();
+        return this;
+    }
+
+    public void genererEtat() {
+        for (int l = 0; l < grille.length; l++) {
+            for (int c = 0; c < grille[0].length; c++) {
+                if (grille[l][c] == joueur)
+                    initGroupes(l, c, null, true);
+                if (grille[l][c] == adversaire)
+                    initGroupes(l, c, null, false);
+            }
+        }
+    }
+
+    private void ajouterGroupe(Groupe groupe, boolean joueur) {
+        if (joueur)
+            listeGroupes.add(groupe);
+        else
+            listeGroupesAdversaire.add(groupe);
+    }
+
+    private Noeud[][] getNoeuds(boolean joueur) {
+        if (joueur)
+            return noeuds;
+        else
+            return noeudsAdversaire;
+    }
+
+    private void initGroupes(int ligne, int colonne, Groupe.Type refType, boolean joueur) {
+        ArrayList<Map.Entry<Integer, Groupe.Type>> adjacents = new ArrayList<>();
+
+        Noeud[][] noeuds = getNoeuds(joueur);
+
+        if (ligne > 0) {
+            if (colonne > 0)
+                insertAdjacent(adjacents, ligne - 1, colonne - 1, Groupe.Type.DIAG_GAUCHE, refType, joueur);
+            insertAdjacent(adjacents, ligne - 1, colonne, Groupe.Type.VERTICAL, refType, joueur);
+            if (colonne < noeuds[0].length - 1)
+                insertAdjacent(adjacents, ligne - 1, colonne + 1, Groupe.Type.DIAG_DROITE, refType, joueur);
+        }
+        if (colonne > 0)
+            insertAdjacent(adjacents, ligne, colonne - 1, Groupe.Type.HORIZONTAL, refType, joueur);
+        if (colonne < noeuds[0].length - 1)
+            insertAdjacent(adjacents, ligne, colonne + 1, Groupe.Type.HORIZONTAL, refType, joueur);
+        if (ligne < noeuds.length - 1) {
+            if (colonne > 0)
+                insertAdjacent(adjacents, ligne + 1, colonne - 1, Groupe.Type.DIAG_DROITE, refType, joueur);
+            insertAdjacent(adjacents, ligne + 1, colonne, Groupe.Type.VERTICAL, refType, joueur);
+            if (colonne < noeuds[0].length - 1)
+                insertAdjacent(adjacents, ligne + 1, colonne + 1, Groupe.Type.DIAG_GAUCHE, refType, joueur);
+        }
+
+        Groupe groupe = new Groupe();
+        if (adjacents.isEmpty()) {
+            // System.out.print(refType + " : Noeud " + ligne + "," + colonne + " : ");
+            // afficherAdjacents(adjacents);
+            if (noeuds[ligne][colonne] == null) {
+                groupe.ajouter(ligne, colonne);
+                groupe.setType(Groupe.Type.SINGLE);
+                ajouterGroupe(groupe, joueur);
+            }
+            return;
+        }
+
+        for (Map.Entry<Integer, Groupe.Type> adjacent : adjacents) {
+            int aLigne = adjacent.getKey() / grille[0].length;
+            int aColonne = adjacent.getKey() % grille[0].length;
+            Groupe.Type type = adjacent.getValue();
+
+            // System.out.print(refType + " : Noeud " + ligne + "," + colonne + " : ");
+            // afficherAdjacents(adjacents);
+            groupe = new Groupe();
+            if (noeuds[ligne][colonne] != null && noeuds[ligne][colonne].groupes.containsKey(type)) {
+                groupe = noeuds[ligne][colonne].groupes.get(type);
+                // System.out.println("Recup groupe: " + type);
+            } else {
+                if (noeuds[ligne][colonne] == null) {
+                    noeuds[ligne][colonne] = new Noeud();
+                }
+                groupe.ajouter(ligne, colonne);
+                groupe.setType(type);
+                noeuds[ligne][colonne].ajouterGroupe(groupe);
+                ajouterGroupe(groupe, joueur);
+                // System.out.println("Ajout groupe: " + type);
+            }
+
+            creerGroupe(aLigne, aColonne, groupe, joueur);
+            // displayGroupes();
+            initGroupes(aLigne, aColonne, type, joueur);
+        }
+
+        return;
+    }
+
+    private void creerGroupe(int ligne, int colonne, Groupe courant, boolean joueur) {
+        Noeud[][] noeuds = getNoeuds(joueur);
+
+        if (noeuds[ligne][colonne] != null && noeuds[ligne][colonne].groupes.containsKey(courant.type))
+            return;
+
+        if (noeuds[ligne][colonne] == null) {
+            noeuds[ligne][colonne] = new Noeud();
+        }
+        courant.ajouter(ligne, colonne);
+        noeuds[ligne][colonne].ajouterGroupe(courant);
+    }
+
+    private void insertAdjacent(ArrayList<Map.Entry<Integer, Groupe.Type>> adjacents, int ligne, int colonne,
+            Groupe.Type type, Groupe.Type refType, boolean joueur) {
+        if (checkCase(ligne, colonne, type, refType, joueur))
+            adjacents.add(new SimpleEntry<>(ligne * grille[0].length + colonne, type));
+    }
+
+    private boolean checkCase(int ligne, int colonne, Groupe.Type type, Groupe.Type refType, boolean isJoueur) {
+        int joueur = isJoueur ? this.joueur : this.adversaire;
+
+        if (grille[ligne][colonne] != joueur) // Case n'appartient pas au joueur courant
+            return false;
+        if (refType != null && type != refType) // Case ne correspond pas au groupe courant
+            return false;
+        if (getNoeuds(isJoueur)[ligne][colonne] != null)
+            if (refType != null)
+                return false; // La case ne contient pas le groupe voulu
+        return true;
+    }
+
+    public int evalFunction() {
+        return countGroupes(listeGroupes) - countGroupes(listeGroupesAdversaire);
+    }
+
+    private int countGroupes(ArrayList<Groupe> groupes) {
+        int count = 0;
+        for (Groupe groupe : groupes) {
+            if (groupe.size() == 1)
+                count += 1;
+            if (groupe.size() == 2)
+                count += 10;
+            if (groupe.size() == 3)
+                count += 100;
+            if (groupe.size() == 4)
+                count += 1000;
+            if (groupe.size() == 5)
+                count += 10000;
+        }
+        return count;
+    }
+
+    private void afficherAdjacents(ArrayList<Map.Entry<Integer, Groupe.Type>> adjacents) {
+        if (adjacents.isEmpty()) {
+            System.out.println("0");
+            return;
+        }
+
+        System.out.print("{ ");
+        for (Map.Entry<Integer, Groupe.Type> adjacent : adjacents) {
+            System.out.print(
+                    "[" + adjacent.getKey() / grille[0].length + "," + adjacent.getKey() % grille[0].length + "]");
+        }
+        System.out.println(" }");
+    }
+
+    @Override
+    public int compareTo(Etat autre) {
+        return this.evalFunction() - autre.evalFunction();
+    }
+}
--- JoueurArtificiel.java
+++ JoueurArtificiel.java
@@ -2,6 +2,7 @@ package Connect5Game;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Map;
 import java.util.AbstractMap.SimpleEntry;
@@ -11,19 +12,11 @@ import Connect5Game.Joueur;
 import Connect5Game.Position;
 import Connect5Game.Noeud;
 import Connect5Game.Groupe;
+import Connect5Game.Etat;
 
 public class JoueurArtificiel implements Joueur {
 
-    // Ces attributs sont réinitialisés à chaque appel de getProchainCoup.
-    // L'information n'est donc pas conservée.
     private final Random random = new Random();
-    Noeud[][] noeuds; // Chaque case est un pointeur vers un / des groupes
-    int joueurCourant; // Contient le joueur courant
-    int nbligne; // Nombre max lignes
-    int nbcol; // Nombre max colonnes
-
-    // DEBUG
-    ArrayList<Groupe> listeGroupes;
 
     /**
      * Voici la fonction à modifier. Évidemment, vous pouvez ajouter d'autres
@@ -39,127 +32,52 @@ public class JoueurArtificiel implements Joueur {
     @Override
     public Position getProchainCoup(Grille grille, int delais) {
         ArrayList<Integer> casesvides = new ArrayList<Integer>();
-        nbligne = grille.getData().length;
-        nbcol = grille.getData()[0].length;
+        int count = 0;
+        int nbligne = grille.getData().length;
+        int nbcol = grille.getData()[0].length;
         for (int l = 0; l < nbligne; l++)
-            for (int c = 0; c < nbcol; c++)
+            for (int c = 0; c < nbcol; c++) {
+                if (grille.getData()[l][c] == 1 || grille.getData()[l][c] == 2)
+                    count++;
                 if (grille.getData()[l][c] == 0)
                     casesvides.add(l * nbcol + c);
+            }
 
         int colonne = 0, ligne = 0;
-        noeuds = new Noeud[nbligne][nbcol];
-        // joueurCourant = casesvides.size() % 2;
-        // joueurCourant = joueurCourant + 1;
-        joueurCourant = 1;
 
-        System.out.println("Joueur courant: " + joueurCourant);
+        Etat etatInitial = new Etat(grille.getData(), (count % 2) + 1);
+        etatInitial.genererEtat();
 
-        listeGroupes = new ArrayList<>();
+        displayGroupes(etatInitial.listeGroupes);
+        displayGroupes(etatInitial.listeGroupesAdversaire);
 
-        for (int l = 0; l < nbligne; l++) {
-            for (int c = 0; c < nbcol; c++) {
-                if (grille.getData()[l][c] == joueurCourant)
-                    initGroupes(grille.getData(), l, c, null);
-            }
-        }
+        // System.out.println("Evaluation: " + etatInitial.evalFunction());
 
-        // return new Position(ligne, colonne);
-        displayGroupes();
+        minimax(etatInitial, casesvides);
 
         int choix = random.nextInt(casesvides.size());
         choix = casesvides.get(choix);
         return new Position(choix / nbcol, choix % nbcol);
     }
 
-    private void initGroupes(byte[][] data, int ligne, int colonne, Groupe.Type recType) {
-        ArrayList<Map.Entry<Integer, Groupe.Type>> adjacents = new ArrayList<>();
+    private void minimax(Etat initial, ArrayList<Integer> casesvides) {
+        ArrayList<Etat> listeEtats = new ArrayList<>();
+        int nbcol = initial.grille[0].length;
 
-        if (ligne > 0) {
-            if (colonne > 0 && checkCase(data, ligne - 1, colonne - 1, Groupe.Type.DIAG_GAUCHE, recType))
-                adjacents.add(new SimpleEntry<>((ligne - 1) * nbcol + colonne - 1, Groupe.Type.DIAG_GAUCHE));
-            if (checkCase(data, ligne - 1, colonne, Groupe.Type.VERTICAL, recType))
-                adjacents.add(new SimpleEntry<>((ligne - 1) * nbcol + colonne, Groupe.Type.VERTICAL));
-            if (colonne < noeuds[0].length - 1
-                    && checkCase(data, ligne - 1, colonne + 1, Groupe.Type.DIAG_DROITE, recType))
-                adjacents.add(new SimpleEntry<>((ligne - 1) * nbcol + colonne + 1, Groupe.Type.DIAG_DROITE));
-        }
-        if (colonne > 0 && checkCase(data, ligne, colonne - 1, Groupe.Type.HORIZONTAL, recType))
-            adjacents.add(new SimpleEntry<>((ligne * nbcol) + colonne - 1, Groupe.Type.HORIZONTAL));
-        if (colonne < noeuds[0].length - 1 && checkCase(data, ligne, colonne + 1, Groupe.Type.HORIZONTAL, recType))
-            adjacents.add(new SimpleEntry<>((ligne * nbcol) + colonne + 1, Groupe.Type.HORIZONTAL));
-        if (ligne < noeuds.length - 1) {
-            if (colonne > 0 && checkCase(data, ligne + 1, colonne - 1, Groupe.Type.DIAG_DROITE, recType))
-                adjacents.add(new SimpleEntry<>((ligne + 1) * nbcol + colonne - 1, Groupe.Type.DIAG_DROITE));
-            if (checkCase(data, ligne + 1, colonne, Groupe.Type.VERTICAL, recType))
-                adjacents.add(new SimpleEntry<>((ligne + 1) * nbcol + colonne, Groupe.Type.VERTICAL));
-            if (colonne < noeuds[0].length - 1
-                    && checkCase(data, ligne + 1, colonne + 1, Groupe.Type.DIAG_GAUCHE, recType))
-                adjacents.add(new SimpleEntry<>((ligne + 1) * nbcol + colonne + 1, Groupe.Type.DIAG_GAUCHE));
+        for (int position : casesvides) {
+            Etat courant = new Etat(initial);
+            courant.genererSuccesseur(position / nbcol, position % nbcol);
+            displayGroupes(courant.listeGroupes);
+            listeEtats.add(courant);
         }
 
-        Groupe groupe = new Groupe();
-        if (adjacents.isEmpty()) {
-            System.out.print(recType + " : Noeud " + ligne + "," + colonne + " : ");
-            afficherAdjacents(adjacents);
-            if (noeuds[ligne][colonne] == null) {
-                groupe.ajouter(ligne, colonne);
-                groupe.setType(Groupe.Type.SINGLE);
-                listeGroupes.add(groupe);
-            }
-            return;
-        }
+        Collections.sort(listeEtats);
 
-        for (Map.Entry<Integer, Groupe.Type> adjacent : adjacents) {
-            int aLigne = adjacent.getKey() / nbcol;
-            int aColonne = adjacent.getKey() % nbcol;
-            Groupe.Type type = adjacent.getValue();
-
-            System.out.print(recType + " : Noeud " + ligne + "," + colonne + " : ");
-            afficherAdjacents(adjacents);
-            groupe = new Groupe();
-            if (noeuds[ligne][colonne] != null && noeuds[ligne][colonne].groupes.containsKey(type)) {
-                groupe = noeuds[ligne][colonne].groupes.get(type);
-                System.out.println("Recup groupe: " + type);
-            } else {
-                if (noeuds[ligne][colonne] == null) {
-                    noeuds[ligne][colonne] = new Noeud();
-                }
-                groupe.ajouter(ligne, colonne);
-                groupe.setType(type);
-                noeuds[ligne][colonne].ajouterGroupe(groupe);
-                listeGroupes.add(groupe);
-                System.out.println("Ajout groupe: " + type);
-            }
-
-            creerGroupe(data, aLigne, aColonne, groupe);
-            displayGroupes();
-            initGroupes(data, aLigne, aColonne, type);
-        }
+        System.out.println(listeEtats.size());
+        // System.out.println("best: " + listeEtats.get(0).evalFunction());
     }
 
-    private void creerGroupe(byte[][] data, int ligne, int colonne, Groupe courant) {
-        if (noeuds[ligne][colonne] != null && noeuds[ligne][colonne].groupes.containsKey(courant.type))
-            return;
-
-        if (noeuds[ligne][colonne] == null) {
-            noeuds[ligne][colonne] = new Noeud();
-        }
-        courant.ajouter(ligne, colonne);
-        noeuds[ligne][colonne].ajouterGroupe(courant);
-    }
-
-    private boolean checkCase(byte[][] data, int ligne, int colonne, Groupe.Type type, Groupe.Type recType) {
-        if (data[ligne][colonne] != joueurCourant) // Case n'appartient pas au joueur courant
-            return false;
-        if (recType != null && type != recType)
-            return false;
-        if (noeuds[ligne][colonne] != null)
-            if (recType != null)
-                return false;
-        return true;
-    }
-
-    private void displayGroupes() {
+    private void displayGroupes(ArrayList<Groupe> listeGroupes) {
         if (listeGroupes == null)
             return;
 
@@ -172,18 +90,5 @@ public class JoueurArtificiel implements Joueur {
             System.out.println(" }");
         }
         System.out.println("-------------------------");
-    }
-
-    private void afficherAdjacents(ArrayList<Map.Entry<Integer, Groupe.Type>> adjacents) {
-        if (adjacents.isEmpty()) {
-            System.out.println("0");
-            return;
-        }
-
-        System.out.print("{ ");
-        for (Map.Entry<Integer, Groupe.Type> adjacent : adjacents) {
-            System.out.print("[" + adjacent.getKey() / nbcol + "," + adjacent.getKey() % nbcol + "]");
-        }
-        System.out.println(" }");
     }
 }
